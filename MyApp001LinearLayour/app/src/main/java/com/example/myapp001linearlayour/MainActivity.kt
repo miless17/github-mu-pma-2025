f73f1f01c4cd1760fd93968389d89027990d9bb3
miless17 <81700551+miless17@users.noreply.github.com>
2025-10-15 09:44:01 +0200

    Ukol 1 commit

---
+++ MyApp001LinearLayour/app/src/main/java/com/example/myapp001linearlayour/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.myapp001linearlayour
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        } */
+
+        val etName = findViewById<EditText>(R.id.etName)
+        val etSurname = findViewById<EditText>(R.id.etSurname)
+        val etPlace = findViewById<EditText>(R.id.etPlace)
+        val etAge = findViewById<EditText>(R.id.etAge)
+        val tvInformation = findViewById<TextView>(R.id.tvInformation)
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        val btnDelete = findViewById<Button>(R.id.btnDelete)
+
+        // Nastavení obsluhy pro tlačítko odeslat
+
+        btnSend.setOnClickListener {
+            val name = etName.text.toString()
+            val surname = etSurname.text.toString()
+            val place = etPlace.text.toString()
+            val age = etAge.text.toString()
+
+            // Zobrazení textu v TextView
+            val formatedText = "Jmenuji se $name $surname a je mi $age let. Moje bydliště je $place."
+
+            tvInformation.text = formatedText
+
+        }
+
+        btnDelete.setOnClickListener {
+            tvInformation.text = ""
+            etName.text.clear()
+            etSurname.text.clear()
+            etAge.text.clear()
+            etPlace.text.clear()
+
+        }
+    }
+}
